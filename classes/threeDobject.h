80aa29c89b40ba6a84fc84ba4e23dd06deb2a05f
suresh1710 <36485119+suresh1710@users.noreply.github.com>
2018-03-04 19:13:36 +0530

    Create threeDobject.h

---
+++ classes/threeDobject.h
@@ -0,0 +1,9 @@
+#include<Vertices.h>
+#include<Line.h>
+#inculde<Plane.h>
+class ThreeDobject{
+          public :vector <vertex3> v;
+                  vector <Line3>   e;
+                  vector <plane3>  f;
+
+}
