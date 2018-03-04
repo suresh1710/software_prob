b066d1a06d858328a965cb780092407f34fa6a3f
prasanth_official <prashanthkurmapu@gmail.com>
2018-03-04 18:48:08 +0530

    created Vertex2 and Vertex3 classes

---
+++ classes/file1.h
@@ -0,0 +1,19 @@
+
+
+class Vertex2{
+    public:
+            double first,second;
+            char[2] axis;// {'x','y'} or {'y','z'} or {'z','x'}
+            
+            
+
+
+}
+
+class Vertex3{
+    public:
+            double first,second,third;
+            Vertex2 getProjection(char* p);
+            
+
+}
